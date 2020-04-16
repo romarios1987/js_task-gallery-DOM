43b309a8b9d9741ed3e1c873b03731665143925b
romarios1987 <romios047@gmail.com>
2020-04-16 08:53:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
+
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', e => {
+  e.preventDefault();
+
+  const thumbLink = event.target.closest('.list-item__link');
+
+  if (!thumbLink) {
+    return;
+  }
+
+  largeImg.src = thumbLink.href;
+  largeImg.alt = thumbLink.title;
+});
